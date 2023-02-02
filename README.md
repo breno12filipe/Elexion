90c25d431b84e256c28f65abdb254e44610615ec
breno12filipe <breno.filipe@sempreceub.com>
2023-02-02 20:46:38 -0300

    Adding admin class

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Elexion
 
+This project follows the Google C++ Style Guide
+
+https://google.github.io/styleguide/cppguide.html
+
 ![alt text](https://itsourcecode.com/wp-content/uploads/2021/11/UML-Class-Diagram-for-Bank-Management-System.png)
---
+++ entities/admin.h
@@ -0,0 +1,21 @@
+#ifndef ELEXION_ENTITIES_ADMIN_H_
+#define ELEXION_ENTITIES_ADMIN_H_
+#include <string>
+
+class Admin {
+ private:
+  std::string username;
+  std::string password;
+
+ protected:
+  int id;
+
+ public:
+  Admin() = default;
+  ~Admin() = default;
+  std::string name;
+  std::string username;
+  std::string contactNumber;
+};
+
+#endif  // ELEXION_ENTITIES_ADMIN_H_
